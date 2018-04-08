d378c8cdc5e40052c9988ab0e1ca3a49a7ce3fbc
Ali Mohammadpour <alimpk@outlook.com>
2018-04-08 10:10:29 +0430

    custom writer example added to http request

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("https://google.com")
@@ -12,12 +15,26 @@ func main() {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
+	/*
+		fmt.Println(resp)
+		fmt.Println("--------------------------")
+		----------------------------------------*/
+	/*
+		bs := make([]byte, 9999)
+		resp.Body.Read(bs)
+		fmt.Println(bs)
+		fmt.Println("--------------------------")
+		----------------------------------------*/
 
-	fmt.Println(resp)
-	fmt.Println("--------------------------")
+	lw := logWriter{}
 
-	bs := make([]byte, 9999)
-	resp.Body.Read(bs)
-	fmt.Println(bs)
+	io.Copy(lw, resp.Body)
 
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+
+	return len(bs), nil
 }
